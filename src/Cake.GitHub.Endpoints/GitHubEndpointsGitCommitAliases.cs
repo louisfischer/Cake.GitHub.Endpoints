0d9bc1075a44d7280326f075a4d7e14860777e25
Louis Fischer <louisfischer@gmail.com>
2023-09-09 16:39:43 -0500

    Fixed misspelled methods (#3)

--- src/Cake.GitHub.Endpoints/GitHubEndpointsGitCommitAliases.cs
+++ src/Cake.GitHub.Endpoints/GitHubEndpointsGitCommitAliases.cs
@@ -1,7 +1,7 @@
 ﻿namespace Cake.GitHub.Endpoints;
 
 /// <summary>
-/// Contains functionality for working withGitHub's Git Database API. Gives you access to read and write raw Git objects and to list and update your references.
+/// Contains functionality for working with GitHub's Git Database API. Gives you access to read and write raw Git objects and to list and update your references.
 /// </summary>
 [CakeAliasCategory("GitHub")]
 [CakeNamespaceImport("Cake.GitHub.Endpoints")]
@@ -12,27 +12,20 @@ public static class GitHubEndpointsGitCommitAliases
     /// </summary>
     /// <param name="context">The GitHubEndpointContext</param>
     /// <param name="reference">Tha sha reference of the commit</param>
-    /// <returns><see cref="Blob"/></returns>
+    /// <returns><see cref="Commit"/></returns>
     /// <remarks>
-    ///  API: <seealso href="http://developer.github.com/v3/git/blobs/#get-a-blob">http://developer.github.com/v3/git/blobs/#get-a-blob</seealso>
+    ///  API: <seealso href="http://developer.github.com/v3/git/commits/#get-a-commit">http://developer.github.com/v3/git/commits/#get-a-commit</seealso>
     /// </remarks>
-    public static Task<Commit> GitHubGitBlob(this IGitHubEndpointContext context, string reference) =>
-        context.GitHubClient().Git.Commit.Get(context.Owner, context.RepoName, reference);
+    public static Task<Commit> GitHubGitCommit(this IGitHubEndpointContext context, string reference) => context.GitHubClient().Git.Commit.Get(context.Owner, context.RepoName, reference);
 
     /// <summary>
-    /// Creates a new Blob
+    /// Create a commit for a given repository
     /// </summary>
     /// <param name="context">The GitHubEndpointContext</param>
-    /// <param name="content">The content of the blob.</param>
-    /// <param name="encoding">The encoding of the blob.</param>
+    /// <param name="commit">The commit to create</param>
     /// <returns><see cref="BlobReference"/></returns>
     /// <remarks>
-    ///  API: <seealso href="http://developer.github.com/v3/git/blobs/#create-a-blob">http://developer.github.com/v3/git/blobs/#create-a-blob</seealso>
+    ///  API: <seealso href="http://developer.github.com/v3/git/commits/#create-a-commit">http://developer.github.com/v3/git/commits/#create-a-commit</seealso>
     /// </remarks>
-    public static Task<BlobReference> GitHubGitTagCreate(this IGitHubEndpointContext context, string content, EncodingType encoding) =>
-        context.GitHubClient().Git.Blob.Create(context.Owner, context.RepoName, new NewBlob
-        {
-             Content = content,
-             Encoding = encoding
-        });
+    public static Task<Commit> GitHubGitCommitCreate(this IGitHubEndpointContext context, NewCommit commit) => context.GitHubClient().Git.Commit.Create(context.Owner, context.RepoName, commit);
 }
